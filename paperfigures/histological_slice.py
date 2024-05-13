611f67479bb5a99703e764ff189393d70cb317c5
Arstanbek Okenov <aokenov@ugent.be>
2024-05-13 15:24:05 +0200

    Update slice figure

--- paperfigures/histological_slice.py
+++ paperfigures/histological_slice.py
@@ -13,7 +13,7 @@ cmap = colors.LinearSegmentedColormap.from_list(
                          (0.5, '#e2a858'),
                          (1, '#990102')])
 
-path = Path('/Users/arstanbek/Hulk/Arstan/analysis/data')
+path = Path(__file__).parents[1].joinpath('data')
 
 hearts = ['E10615_MYH7', 'E10621_ABCC9', 'E10691_RBM20', 'E10788_LMNA',
           'E10884', 'E10927_MYBPC3', 'E11442_TTN', 'E11443_LMNA',
@@ -46,30 +46,60 @@ n_r_segments = 6
 n_a_segments = 36
 
 
-x0 = [[1400, 1128], [1227, 898]]
-y0 = [[2313, 2858], [2192, 2729]]
+# x0 = [[1400, 1128], [1227, 898]]
+# y0 = [[2313, 2858], [2192, 2729]]
 
-fig, ax = plt.subplots(ncols=2, figsize=(10, 4))
-ax[0].imshow(image, cmap=cmap, origin='lower')
-ax[0].plot(edges['endo'].full_nodes[:, 1], edges['endo'].full_nodes[:, 0],
+x0 = 3000
+dx0 = 200
+y0 = 1650
+dy0 = 200
+
+x1 = 3100
+dx1 = 60
+y1 = 1745
+dy1 = 60
+# x1 = [[1400, 1128], [1227, 898]]
+
+fig, ax = plt.subplots(ncols=3, nrows=2, figsize=(10, 4))
+
+ax[0, 0].imshow(image, cmap=cmap, origin='lower')
+ax[0, 0].plot(edges['endo'].full_nodes[:, 1], edges['endo'].full_nodes[:, 0],
            '#1f77b4', lw=2)
-ax[0].plot(edges['epi'].full_nodes[:, 1], edges['epi'].full_nodes[:, 0],
+ax[0, 0].plot(edges['epi'].full_nodes[:, 1], edges['epi'].full_nodes[:, 0],
            '#2ca02c', lw=2)
 
-rect = patches.Rectangle((850, 2250), 200, 200, linewidth=1, edgecolor='black',
+rect = patches.Rectangle((x0, y0), dx0, dy0, linewidth=1, edgecolor='black',
                          facecolor='none')
+ax[0, 0].add_patch(rect)
+ax[0, 0].set_title('A', loc='left', fontsize=16)
+ax[0, 0].axis('off')
 
-ax[0].add_patch(rect)
+ax[0, 1].imshow(image, cmap=cmap, origin='lower')
+ax[0, 1].set_ylim(y0, y0 + dy0)
+ax[0, 1].set_xlim(x0, x0 + dx0)
 
-ax[1].imshow(image, cmap=cmap)
-ax[1].set_ylim(2250, 2450)
-ax[1].set_xlim(850, 1050)
+ax[0, 1].set_title('B', loc='left', fontsize=16)
+ax[0, 1].set_xticks([])
+ax[0, 1].set_yticks([])
 
-ax[0].set_title('A', loc='left', fontsize=16)
-ax[1].set_title('B', loc='left', fontsize=16)
-ax[0].axis('off')
-ax[1].set_xticks([])
-ax[1].set_yticks([])
+rect = patches.Rectangle((x1, y1), dx1, dy1, linewidth=1, edgecolor='black',
+                         facecolor='none')
+ax[0, 1].add_patch(rect)
+
+ax[0, 2].imshow(image, cmap=cmap, origin='lower')
+ax[0, 2].set_ylim(y1, y1 + dy1)
+ax[0, 2].set_xlim(x1, x1 + dx1)
+ax[0, 2].set_title('C', loc='left', fontsize=16)
+ax[0, 2].set_xticks([])
+ax[0, 2].set_yticks([])
+
+for i, label in enumerate(['D', 'E', 'F']):
+    ax[1, i].imshow(image, cmap=cmap, origin='lower')
+    ax[1, i].set_ylim(y1, y1 + dy1)
+    ax[1, i].set_xlim(x1, x1 + dx1)
+    ax[1, i].set_title(label, loc='left', fontsize=16)
+    ax[1, i].set_xticks([])
+    ax[1, i].set_yticks([])
 
 plt.tight_layout()
 
